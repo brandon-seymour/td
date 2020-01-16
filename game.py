f7f5db92699fa5bfbeb1b8301b085da523d730fd
brandon-seymour <59863256+brandon-seymour@users.noreply.github.com>
2020-01-15 23:37:13 -0800

    Creating repo

---
+++ game.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+class Game:
+    def __init__(self):
+        self.width = 1000
+        self.height = 700
+        self.win = pygame.display.set_mode((self.width, self.height))
+
+    def run(self):
+        run = True
+
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+        pygame.quit()
+
+Game()
