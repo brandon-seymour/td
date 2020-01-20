9131b377c4dc06730b7a5655335e5e2663bf0f31
brandon-seymour <59863256+brandon-seymour@users.noreply.github.com>
2020-01-19 20:36:03 -0800

    bg draw

--- enemies/enemy.py
+++ enemies/enemy.py
@@ -2,9 +2,11 @@ import pygame
 
 class Enemy:
     imgs = []
-    def __init__(self, x, y):
+    def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
         self.imgs = []
         self.animation_count = 0
         self.health = 1
@@ -13,7 +15,10 @@ class Enemy:
     def draw(self, win):
         pass
 
-    def collide(self, x, y):
+    def collide(self, X, Y):
+        if X <= self.x + self.width and X>=self.x:
+            if Y<= self.y + self.height and Y >= self.y:
+                return True
         # Returns if position has hit enemy
         return False
 
--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame
-
+import os
 
 class Game:
     def __init__(self):
@@ -10,15 +10,24 @@ class Game:
         self.towers = []
         self.lives = 10
         self.money = 100
+        self.bg = pygame.image.load(os.path.join("assets", "bg.png"))
 
     def run(self):
         run = True
+        clock = pygame.time.Clock()
 
         while run:
+            clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
 
+            self.draw()
+
         pygame.quit()
+
+    def draw(self):
+        self.win.blit(self.bg, (0,0))
+        pygame.display.update()
 
 Game()
